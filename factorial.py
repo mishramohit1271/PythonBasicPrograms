d138b6038072caa882567f3bc79a64dbba3a381e
Mohit Mishra <mishra_mohit1@proton.me>
2023-03-20 11:18:18 +0530

    Code for the factorial using Loop

---
+++ factorial.py
@@ -0,0 +1,16 @@
+# Python program to find the factorial of a number provided by the user.
+
+# Take input from the user
+
+num = int(input("Enter a number: "))
+fact = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1, num + 1):
+       fact = fact*i
+   print("The factorial of",num,"is",fact)
